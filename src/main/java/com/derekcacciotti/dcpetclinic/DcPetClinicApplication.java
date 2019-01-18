cad53fa04c31f4dd6b88f820f6aa3b95dd76cef7
derekcacciotti <derekc247@me.com>
2019-01-18 16:06:55 -0500

    inital project add

---
+++ src/main/java/com/derekcacciotti/dcpetclinic/DcPetClinicApplication.java
@@ -0,0 +1,14 @@
+package com.derekcacciotti.dcpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DcPetClinicApplication.class, args);
+    }
+
+}
+
