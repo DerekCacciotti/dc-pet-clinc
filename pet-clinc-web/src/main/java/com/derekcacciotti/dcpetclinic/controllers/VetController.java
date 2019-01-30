f3182baa379c80f58df7002e839ff2e126ac5a37
derekcacciotti <derekc247@me.com>
2019-01-30 14:59:04 -0500

    added vets page closes #7

---
+++ pet-clinc-web/src/main/java/com/derekcacciotti/dcpetclinic/controllers/VetController.java
@@ -0,0 +1,14 @@
+package com.derekcacciotti.dcpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listvets()
+    {
+        return "vets/index";
+    }
+}
